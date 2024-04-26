a5a6c3f22347e105c7ee3a7b8f37a9e1e8ccb9b2
swethadurai <111978148+swethadurai@users.noreply.github.com>
2024-04-26 12:14:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require "dbconnection.php";
+?>
+<table border="1">
+    <tr>
+        <td>id</td>
+        <td>city</td>
+        <td>temperature</td>
+        <td>weather</td>
+        <td>lattitude</td>
+        <td>longitude</td>
+        <td>wind speed</td>
+        <td>humidity	</td>
+    </tr>
+
+<?php
+$i=1;
+$rows=mysqli_query($conn,"SELECT * FROM data");
+foreach ($rows as $row) :
+?>
+<tr>
+    <td><?php echo $i++; ?></td>
+    <td><?php echo $row["city"]; ?></td>
+    <td><?php echo $row["temperature"]; ?></td>
+    <td><?php echo $row["weather"]; ?></td>
+    <td><?php echo $row["lattitude"]; ?></td>
+    <td><?php echo $row["longitude"]; ?></td>
+    <td><?php echo $row["wind speed"]; ?></td>
+    <td><?php echo $row["humidity"]; ?></td>
+</tr>
+<?php endforeach;?>
+</table>
